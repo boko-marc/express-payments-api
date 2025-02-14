d2e7eab69730f4a5d5b5e54bc2e4a56ffee63746
boko-marc <marcboko.uriel@gmail.com>
2025-02-14 18:33:44 +0100

    Update database connection URI and log message in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
-    console.log("✅ Database is running !");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ Connected to the database ${process.env.MONGO_URI}!`);
   } catch (error) {
     console.error("❌ Error when connecting to database :", error);
     process.exit(1);
