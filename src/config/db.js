4c5e2b9bf978e2cf838cba068dac159f6584baf5
boko-marc <marcboko.uriel@gmail.com>
2025-02-16 10:50:46 +0100

    Set up Docker environment with Dockerfile and docker-compose, update MongoDB connection in db.js, and add .env.example

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const mongoURI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(mongoURI);
     console.log(`✅ Connected to the database ${process.env.MONGO_URI}!`);
   } catch (error) {
     console.error("❌ Error when connecting to database :", error);
